82a0260a166b1123c096d2ab1d98f0ea836968fd
Harold C <hcabalic@gmail.com>
2023-08-02 01:15:04 -0700

    Swap alpha logic for CSS opacity transition

--- src/components/Visualizer.jsx
+++ src/components/Visualizer.jsx
@@ -1,32 +1,24 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import "../styles/visualizer.scss";
 
 const initCanvas = (canvas) => {
   if (!canvas) return;
-  
-  const [width, height] = [1280, 862];
+
+  // TODO: responsive canvas dimensions
+  const [width, height] = [1152, 776];
   const context = canvas.getContext("2d");
   context.canvas.width = width;
   context.canvas.height = height;
-  context.globalAlpha = 0;
   return context;
 }
 
 const imageLoader = (context, image) => {
-  const [width, height] = [1280, 862]
+  const [width, height] = [1152, 776]
   context.drawImage(image, 0, 0, width, height);
-  
-  const intervalID = setInterval(() => {
-    if (context.globalAlpha > .9) {
-      clearInterval(intervalID);
-    }
-    context.globalAlpha += 0.075;
-    context.clearRect(0, 0, width, height)
-    context.drawImage(image, 0, 0, width, height);
-  }, 100);
 }
 
 const Visualizer = () => {
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
   const canvasRef = useRef(null);
 
   useEffect(() => {
@@ -36,14 +28,15 @@ const Visualizer = () => {
     image.onload = () => {
       const context = initCanvas(canvasRef.current, image);
       imageLoader(context, image);
+      setIsImageLoaded(true);
     }
     image.src = "./src/assets/images/dan-meyers.jpg";
   }, []);
 
   return (
     <div className="container">
-      <canvas id="canvas" ref={canvasRef} className="canvas">
-        <img className="canvas" src="./src/assets/images/dan-meyers.jpg" loading="lazy" />
+      <canvas id="canvas" ref={canvasRef} className={`canvas ${isImageLoaded ? 'loaded' : ''}`}>
+        <img className="canvas" src="./src/assets/images/dan-meyers-1152.jpg" loading="lazy" />
       </canvas>
     </div>
   )
